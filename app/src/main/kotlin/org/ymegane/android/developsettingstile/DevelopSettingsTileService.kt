458ab08894cfcef66df5755f61f4a2590291472f
y_megane <crybaby.sumire+github@gmail.com>
2016-06-19 11:46:09 +0900

    Initial commit

---
+++ app/src/main/kotlin/org/ymegane/android/developsettingstile/DevelopSettingsTileService.kt
@@ -0,0 +1,81 @@
+package org.ymegane.android.developsettingstile
+
+import android.content.Intent
+import android.database.ContentObserver
+import android.os.Handler
+import android.provider.Settings
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+import android.util.Log
+
+/**
+ * Provide for the TileService events
+ */
+class DevelopSettingsTileService : TileService() {
+    private val TAG = "DevelopSettingsTileService"
+
+    private var developSettingsObserver : DevelopSettingsObserver? = null;
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return START_STICKY
+    }
+
+    override fun onClick() {
+        Log.d(TAG, "onClick")
+
+        val intent = Intent("android.settings.APPLICATION_DEVELOPMENT_SETTINGS")
+        intent.addCategory(Intent.CATEGORY_DEFAULT)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+
+        sendBroadcast(Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS));
+    }
+
+    override fun onTileAdded() {
+        super.onTileAdded()
+        Log.d(TAG, "onTileAdded")
+    }
+
+    override fun onTileRemoved() {
+        super.onTileRemoved()
+        Log.d(TAG, "onTileRemoved")
+    }
+
+    override fun onStartListening() {
+        super.onStartListening()
+        Log.d(TAG, "onStartListening")
+        developSettingsObserver = DevelopSettingsObserver(Handler())
+        contentResolver.notifyChange(Settings.Global.getUriFor(Settings.Global.DEVELOPMENT_SETTINGS_ENABLED), developSettingsObserver)
+
+        updateAppTile()
+    }
+
+    override fun onStopListening() {
+        super.onStopListening()
+        Log.d(TAG, "onStopListening")
+
+        if (developSettingsObserver != null) {
+            contentResolver.unregisterContentObserver(developSettingsObserver)
+            developSettingsObserver = null
+        }
+    }
+
+    fun updateAppTile() {
+        val enabled = Settings.Global.getInt(contentResolver, Settings.Global.DEVELOPMENT_SETTINGS_ENABLED)
+        Log.d(TAG, "DEVELOPMENT_SETTINGS_ENABLED = %d".format(enabled))
+
+        val tile = qsTile
+        tile.state = if (enabled == 1) Tile.STATE_ACTIVE else Tile.STATE_INACTIVE
+        tile.updateTile()
+    }
+
+    /**
+     * Receive change of develop settings
+     */
+    inner class DevelopSettingsObserver(handler: Handler) : ContentObserver(handler) {
+        override fun onChange(selfChange: Boolean) {
+            super.onChange(selfChange)
+            updateAppTile()
+        }
+    }
+}
